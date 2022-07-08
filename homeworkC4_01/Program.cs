f4d74d2165086c910003dfadee885b2dd5785786
Dmitriy <rusich063@gmail.com>
2022-07-08 11:58:00 +0300

    Домашнее задание 4 семинар

---
+++ homeworkC4_01/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+int Prompt(string message)
+{
+    Console.Write(message);
+    int a = int.Parse(Console.ReadLine());
+    return a;
+}
+int step(int numberA, int numberB){
+  int result = 1;
+  for(int i=1; i <= numberB; i++){
+    result = result * numberA;
+  }
+    return result;
+}
+int numberA = Prompt("Введите a: ");
+int numberB = Prompt("Введите b: ");
+int result1 = step(numberA, numberB);
+Console.WriteLine("Ответ: " + result1);
+
+
+
---
+++ homeworkC4_02/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+int Prompt(string message)
+{
+    Console.Write(message);
+    int a = int.Parse(Console.ReadLine());
+    return a;
+}
+
+int SumDigits(int number)
+{
+    int sum = 0;
+    while (number > 0)
+    {
+        sum += number % 10;
+        number = number / 10;
+    }
+    return sum;
+}
+int number = Prompt("Введите A: ");
+int result = SumDigits(number);
+System.Console.WriteLine(result);
